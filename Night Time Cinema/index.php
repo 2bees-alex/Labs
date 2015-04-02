77540193698bf171d8469061d1ba5c6d7d21ac4e
Lee Nolan <hello@vrsites.com>
2015-04-02 14:19:09 +0200

    Night time cinema
    Initial commit

---
+++ Night Time Cinema/index.php
@@ -0,0 +1,57 @@
+<FireBoxRoom>
+<Assets>
+<AssetObject id="outterlayer" src="outterlayer.obj" tex0="00001.png" />
+<AssetObject id="grass" src="grass.obj" tex0="grass.jpg" />
+<AssetObject id="body1" src="body1.obj" mtl="body1.mtl" />
+<AssetObject id="body3" src="body3.obj" mtl="body3.mtl" />
+<AssetObject id="tree" src="tree.obj" tex0="tree_texture.png" />
+<AssetObject id="body2" src="body2.obj" mtl="body2.mtl" />
+<AssetObject id="moon" src="moon.obj" tex0="moon.png" />
+<AssetObject id="car_coll" src="car_coll.obj" />
+<AssetObject id="screen1" src="viewing_screen.obj" />
+<AssetImage id="sky_back" src="black.jpg" tex_clamp="true" />
+<AssetImage id="sky_down" src="black.jpg" tex_clamp="true" />
+<AssetImage id="sky_up" src="black.jpg" tex_clamp="true" />
+<AssetImage id="sky_front" src="black.jpg" tex_clamp="true" />
+<AssetImage id="sky_left" src="black.jpg" tex_clamp="true" />
+<AssetImage id="sky_right" src="black.jpg" tex_clamp="true" />
+<AssetImage id="howtocine" src="howtocinema.jpg" />
+<AssetWebSurface id="connectcast" src="http://sync-video.com/tv/VRSites" />
+</Assets>
+<Room use_local_asset="room_plane" visible="false" pos="-36.000000 0.000000 -5.000000" xdir="0.000000 0.000000 -1.000000" ydir="0.000000 1.000000 0.000000" zdir="1.000000 0.000000 0.000000" col="1.000000 1.000000 1.000000" skybox_down_id="sky_down" skybox_front_id="sky_front" skybox_left_id="sky_left" skybox_back_id="sky_back" skybox_right_id="sky_right" skybox_up_id="sky_up" near_dist="0.010000" far_dist="10000.000000" locked="true">
+<Object id="body3" js_id="28" pos="-13.800000 0.000000 4.500000" xdir="-0.052 0.000 -0.999" zdir="0.999 0.000 -0.052" scale="1.300000 1.300000 1.300000" col="0.650 0.670 0.530" />
+<Object id="body1" js_id="11" pos="-14.300000 0.000000 -0.300000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.390 0.590 0.350" collision_id="car_coll" />
+<Object id="tree" js_id="40" pos="-12.099999 -2.100000 26.000000" xdir="-0.500 0.000 -0.866" zdir="0.866 0.000 -0.500" scale="8.000000 8.000000 8.000000" col="0.150 0.150 0.150" lighting="false" cull_face="none" />
+<Object id="tree" js_id="45" pos="20.000000 -2.500000 2.000000" xdir="-0.707 0.000 0.707" zdir="-0.707 0.000 -0.707" scale="9.000000 9.000000 9.000000" col="0.150 0.150 0.150" lighting="false" cull_face="none" />
+<Object id="body3" js_id="37" pos="-14.300000 0.000000 -0.300000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.400 0.400 0.350" />
+<Object id="tree" js_id="29" pos="-10.100000 -2.100000 -27.000000" scale="8.000000 8.000000 8.000000" col="0.150 0.150 0.150" lighting="false" cull_face="none" />
+<Object id="body2" js_id="41" pos="-4.800000 0.000000 5.500000" xdir="-0.087 0.000 -0.996" zdir="0.996 0.000 -0.087" scale="1.300000 1.300000 1.300000" col="0.430 0.410 0.500" />
+<Object id="tree" js_id="25" pos="21.000000 -1.000000 -13.000000" xdir="-1.000 0.000 0.000" zdir="0.000 0.000 -1.000" scale="9.000000 9.000000 9.000000" col="0.150 0.150 0.150" lighting="false" cull_face="none" />
+<Object id="body3" js_id="23" pos="-5.800000 0.000000 -3.800000" xdir="0.087 0.000 -0.996" zdir="0.996 0.000 0.087" scale="1.300000 1.300000 1.300000" col="0.500 0.410 0.500" />
+<Object id="body2" js_id="34" pos="-14.300000 0.000000 -0.300000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.400 0.400 0.350" />
+<Object id="body1" js_id="16" pos="-14.800000 0.000000 -4.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.790 0.400 0.400" collision_id="car_coll" />
+<Object id="body1" js_id="4" pos="-5.800000 0.000000 -3.800000" xdir="0.087 0.000 -0.996" zdir="0.996 0.000 0.087" scale="1.300000 1.300000 1.300000" col="0.760 0.740 0.500" collision_id="car_coll" />
+<Object id="cylinder" js_id="7" pos="13.000000 0.000000 5.000000" scale="1.000000 5.000000 1.000000" col="0.100 0.100 0.100" />
+<Object id="grass" js_id="21" xdir="0.000 0.000 -1.000" ydir="0.000 -1.000 0.000" zdir="-1.000 0.000 0.000" scale="5.000000 1.000000 5.000000" col="0.830 0.750 0.770" cull_face="none" collision_id="grass" />
+<Object id="body1" js_id="42" pos="-4.800000 0.000000 5.500000" xdir="-0.087 0.000 -0.996" zdir="0.996 0.000 -0.087" scale="1.300000 1.300000 1.300000" col="0.800 0.800 0.600" collision_id="car_coll" />
+<Object id="body2" js_id="46" pos="-5.800000 0.000000 -3.800000" xdir="0.087 0.000 -0.996" zdir="0.996 0.000 0.087" scale="1.300000 1.300000 1.300000" col="0.700 0.700 0.500" />
+<Object id="body3" js_id="48" pos="-5.400000 0.000000 0.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.500 0.540 0.500" />
+<Object id="tree" js_id="8" pos="23.000000 -1.000000 16.000000" scale="9.000000 9.000000 9.000000" col="0.150 0.150 0.150" lighting="false" cull_face="none" />
+<Object id="body1" js_id="15" pos="-13.800000 0.000000 4.500000" xdir="-0.052 0.000 -0.999" zdir="0.999 0.000 -0.052" scale="1.300000 1.300000 1.300000" col="0.490 0.330 0.130" collision_id="car_coll" />
+<Object id="moon" js_id="10" pos="-539.000000 247.000000 242.000000" xdir="0.052 0.000 -0.999" zdir="0.999 0.000 0.052" scale="50.000000 50.000000 50.000000" col="0.500 0.360 0.320" lighting="false" />
+<Object id="outterlayer" js_id="32" xdir="-1.000 0.000 0.000" zdir="0.000 0.000 -1.000" scale="50.000000 50.000000 50.000000" lighting="false" />
+<Image id="howtocine" js_id="1" pos="-29.000000 1.000000 -4.000000" xdir="0.000 0.000 1.000" ydir="0.259 0.966 0.000" zdir="-0.966 0.259 0.000" />
+<Object id="screen1" js_id="31" pos="5.700000 11.800000 15.600000" xdir="-1.000 0.000 0.000" ydir="0.000 0.000 -1.000" zdir="0.000 -1.000 0.000" scale="1.000000 4.100000 1.300000" lighting="false" websurface_id="connectcast" />
+<Object id="body3" js_id="2" pos="-4.800000 0.000000 5.500000" xdir="-0.087 0.000 -0.996" zdir="0.996 0.000 -0.087" scale="1.300000 1.300000 1.300000" />
+<Object id="tree" js_id="18" pos="3.200000 -2.000000 24.900000" xdir="0.000 0.000 1.000" zdir="-1.000 0.000 0.000" scale="5.000000 5.000000 5.000000" col="0.250 0.250 0.250" lighting="false" cull_face="none" />
+<Object id="body1" js_id="36" pos="-5.400000 0.000000 0.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.660 0.800 0.490" collision_id="car_coll" />
+<Object id="tree" js_id="27" pos="2.200000 -1.300000 -21.000000" xdir="-0.766 0.000 -0.643" zdir="0.643 0.000 -0.766" scale="7.000000 7.000000 7.000000" col="0.250 0.250 0.250" lighting="false" cull_face="none" />
+<Object id="tree" js_id="3" pos="14.000000 -2.600000 -21.600000" xdir="0.000 0.000 1.000" zdir="-1.000 0.000 0.000" scale="9.000000 9.000000 9.000000" col="0.350 0.350 0.350" lighting="false" cull_face="none" />
+<Object id="body3" js_id="19" pos="-14.800000 0.000000 -4.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.660 0.580 0.660" />
+<Object id="body2" js_id="26" pos="-13.800000 0.000000 4.500000" xdir="-0.052 0.000 -0.999" zdir="0.999 0.000 -0.052" scale="1.300000 1.300000 1.300000" col="0.400 0.350 0.400" />
+<Object id="body2" js_id="14" pos="-14.800000 0.000000 -4.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.690 0.400 0.540" />
+<Object id="cylinder" js_id="39" pos="13.000000 0.000000 -5.000000" scale="1.000000 5.000000 1.000000" col="0.100 0.100 0.100" />
+<Object id="body2" js_id="33" pos="-5.400000 0.000000 0.500000" xdir="0.000 0.000 -1.000" zdir="1.000 0.000 0.000" scale="1.300000 1.300000 1.300000" col="0.700 0.800 0.600" />
+<Object id="tree" js_id="9" pos="14.000000 -3.100000 25.900000" xdir="-0.500 0.000 0.866" zdir="-0.866 0.000 -0.500" scale="8.000000 8.000000 8.000000" col="0.350 0.350 0.350" lighting="false" cull_face="none" />
+</Room>
+</FireBoxRoom>
